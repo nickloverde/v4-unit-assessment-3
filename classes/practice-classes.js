74333a83bf3ccc35e79906ec1e5c3e29afbd5a0d
nickloverde <nick.loverde@gmail.com>
2020-11-20 17:06:05 -0700

    Initial Commit

--- classes/practice-classes.js
+++ classes/practice-classes.js
@@ -16,6 +16,16 @@
 
 //CODE HERE
 
+class Character {
+  constructor(name, type){
+    this.name = name
+    this.type = type
+  }
+  getInfo(){
+    return `This is a ${this.type} character named ${this.name}`
+  }
+}
+
 //////////////////PROBLEM 2////////////////////
 
 /*
@@ -34,12 +44,25 @@
 
 //CODE HERE
 
+class NPC extends Character {
+  constructor(name, type, location, phrase){
+    super(name, type)
+    this.location = location
+    this.phrase = phrase
+  }
+  dialogue(){
+    return `${this.name}: ${this.phrase}`
+  }
+}
+
 /*
     Create an NPC named Ralph who is a human located in Niceland. His phrase should be `I'm gonna wreck it!`. 
     Store your new NPC in a variable called 'ralph'.
 */
 
 //CODE HERE
+
+let ralph = new NPC("Ralph", "human", "Niceland",  `I'm gonna wreck it!`)
 
 /*
     Next you're going to create three variables to store information about Ralph.
@@ -49,6 +72,10 @@
 */
 
 //CODE HERE
+
+// let ralphsInfo = ralph.getInfo()
+let ralphsDialogue = ralph.dialogue()
+let ralphsLocation = ralph.location
 
 //////////////////PROBLEM 3////////////////////
 
@@ -75,6 +102,27 @@
 
 //CODE HERE
 
+class Player extends Character {
+  constructor(name, type, healthLevel, attackLevel){
+    super(name, type)
+    this.healthLevel = healthLevel
+    this.attackLevel = attackLevel
+  }
+  // defend(opponentAttackLevel){
+  //   let  this.healthLevel  
+  //   if(this.healthLevel > 3){
+  //     return {
+  //       attackStrength: this.attackLevel,
+  //       remainingHealth: this.healthLevel - this.attackLevel,
+  //       message: `${this.name} is still in the fight`
+  //     }
+  //   } else {
+  //     return `${this.name} has been defeated`
+  //   }
+  // }
+}
+
+
 /*
     Next, we'll create two Players.
     Store the first in a variable called aang, his name should be 'Aang' 
@@ -85,6 +133,9 @@
 
 //CODE HERE
 
+let aang = new Player("Aang", "airbender", 100, 100)
+let ozai = new Player("Ozai", "firebender", 100, 0)
+
 /*
     Let's see how a fight between these two would go. 
     Create a variable called 'battle' whose value is Ozai's 
@@ -93,6 +144,8 @@
 */
 
 //CODE HERE
+
+let battle = ozai.defend(aang.attackLevel)
 
 //////////////////PROBLEM 4////////////////////
 
@@ -112,6 +165,21 @@
 
 //CODE HERE
 
+class Hero extends Player{
+  constructor(name, type, healthLevel, attackLevel, superPowers){
+  super(name, type, healthLevel, attackLevel)
+  this.superPowers = []
+  
+  // addSuperPower(power){
+  //   this.superPowers.push(power)
+  // }
+  // useSuperPower(index){
+  //   this.superPowers.splice(index, 1, `${this.name} used ${this.superPower[index]}`)
+  // }
+  
+  }
+}
+
 /*
   Create a hero named 'Fire Spitter' whose type is 'dragon'. 
   Fire Spitter's healthLevel and attackLevels should both be 5000. 
@@ -122,3 +190,7 @@
 */
 
 //CODE HERE
+
+let fireSpitter = new Player("Fire Spitter", "dragon", 5000, 5000)
+fireSpitter.addSuperPowers("spitting fire", "kickin ass", 'takin names')
+let fireSpitterAttack = fireSpitter.useSuperPower(0)
--- closures/practice-closures.js
+++ closures/practice-closures.js
@@ -10,10 +10,20 @@
   
   //CODE HERE
 
+  function myFunc(){
+    let myStr = 'super secret string'
+    
+    return function getSecretString(){
+      return myStr
+    }
+  }
+
   
   //Now create a variable called secretString. Its value should be the invocation of myFunc.
 
   //CODE HERE
+
+  let secretString = myFunc()
   
   
   ////////////PROBLEM 2////////////
@@ -30,25 +40,44 @@
   
   //CODE HERE
 
+   function lightSwitch(){
+    let isTheLightOn = false
+
+    return function flipTheSwitch(){
+      if(isTheLightOn === true){
+        return 'The light is on'
+      } else {
+        return 'The light is off'
+      }
+    }
+  }
+
   
   //Create a variable called kitchenSwitch whose value is the invocation of lightSwitch.
   
   //CODE HERE
 
+  let kitchenSwitch = lightSwitch()
   
   //Invoke kitchenSwitch.
 
   //CODE HERE
+
+  kitchenSwitch()
   
   
   //Create a variable called bathroomSwitch whose value is the invocation of lightSwitch. 
 
   //CODE HERE
   
+  let bathroomSwitch = lightSwitch()
   
   //Invoke bathroomSwitch twice.
   
   //CODE HERE
+
+  bathroomSwitch()
+  bathroomSwitch()
 
   
   
@@ -65,6 +94,25 @@
 
   //CODE HERE
 
+  function plantTracker(){
+    let plant = 'fern'
+    let height = 12
+
+    return {
+      readInfo: (plant) => {
+      return `This is a ${plant} plant that is ${height} inches tall`
+    },
+    waterplant: () => {
+      height += 1
+      return height
+    },
+    prunePlant: () => {
+      height -= 1
+      return height
+    }
+    }
+  }
+
 
   ////////////PROBLEM 4////////////
 
@@ -80,6 +128,25 @@
 
   //CODE HERE
 
+  function inventory(){
+    let products = []
+    let product = ''
+
+    return {
+      readProducts: () => {
+      return products
+    },
+    addToProducts: (product) => {
+      products.push(product)
+      return products
+    },
+    deleteFromProducts: (product) => {
+      products.splice()
+      return products
+    }
+    }
+  }
+
 
   /*
     Create a variable called 'shoes' whose value is the invocation of inventory.
@@ -87,10 +154,14 @@
 
   //CODE HERE
 
+  let shoes = inventory()
+
 
   /*
     Add an item to your shoes array using the addToProducts function
   */
 
   //CODE HERE
+
+  shoes.addToProducts("Louis Vutton")
 
